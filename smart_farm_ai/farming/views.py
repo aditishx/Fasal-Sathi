c649161da263e84bc843304f7e4afca43911b0dc
Aditi <aditi2805sharma@gmail.com>
2026-02-01 20:16:57 +0530

    Fix farming app import and structure

--- smart_farm_ai/farming/views.py
+++ smart_farm_ai/farming/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponse
 
 # from django.contrib.auth.models import UserProfile
 from django.core.files.storage import FileSystemStorage
--- smart_farm_ai/smart_farm_ai/settings.py
+++ smart_farm_ai/smart_farm_ai/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'smart_farm_ai.farming.apps.FarmingConfig',
+    'farming',
 ]
 
 MIDDLEWARE = [
